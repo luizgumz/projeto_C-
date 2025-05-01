5b04e5e4a7a9f9efc4d3be82b68017d463033b32
Luiz Gumz <luiz0augusto0gumz@gmail.com>
2025-05-01 18:31:08 -0300

    feat: Exercicio4 final

---
+++ projeto_C-/exercicio7/exercicio7.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Threading;
+
+namespace projeto_C_.exercicio4
+{
+    abstract class Exercicio07
+    {
+        public static void Execute() 
+        {
+
+        }
+    }
+}
