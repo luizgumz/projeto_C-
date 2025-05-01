4d204db045bc659145b8b4ae77ca1bb866c04a78
Luiz Gumz <luiz0augusto0gumz@gmail.com>
2025-04-30 21:05:43 -0300

    a

---
+++ projeto_C-/exercicio4/exercicio4.cs
@@ -0,0 +1,30 @@
+
+namespace projeto_C_.exercicio4
+{
+    abstract class Exercicio04
+    {
+        public static void Execute() 
+        {
+            while(true)
+            {
+                try 
+                {
+                    Console.WriteLine("=== Conversor de temperaturas === ");
+                    Console.WriteLine("1. Celsius para Fahrenheit");
+                    Console.WriteLine("2. Fahrenheit para Celsius");
+                    Console.WriteLine("3. Sair")
+
+                    Console.WriteLine("")
+                }
+                catch
+                {
+
+                }
+                finally
+                {
+
+                }
+            }
+        }
+    }
+}
